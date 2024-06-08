ec20bacbda0ab066d14699e165111488ade24a0e
SEN SIUUU <157281835+faa35@users.noreply.github.com>
2024-06-08 00:02:21 -0700

    init

---
+++ src/main/java/com/example/demorectangles/models/RectangleRepository.java
@@ -0,0 +1,6 @@
+package com.example.demorectangles.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RectangleRepository extends JpaRepository<Rectangle, Long> {
+}
